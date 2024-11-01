d8ad522f83df09572b59fa67d2754d82e29c4c65
MarcellGranat <granatcellmar98@gmail.com>
2024-11-01 13:24:50 +0100

    direct naming

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="pyRM",
     version="0.1",
-    packages=find_packages(),
+    packages=["pyRM"],
     install_requires=[
         "pyhere>=1.0.0"      # specify the minimum version if needed
     ],
